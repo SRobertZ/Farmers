39159da3eeeb389c06aaa12979f33ee4e4f3e7c9
srobertz <robert.sabiryanov@gmail.com>
2014-03-10 21:23:06 +0600

    date and VK

--- frontEnd/scripts/directives/forum.js
+++ frontEnd/scripts/directives/forum.js
@@ -2,7 +2,12 @@ myApp.directive('forum', function(){
 	return {
 		restrict:'A',
 		replace:true,
-		templateUrl:"partials/forum.html"
+		templateUrl:"partials/forum.html",
+        controller:function()
+        {
+            VK.init({ apiId: 4234459, onlyWidgets: true });
+            VK.Widgets.Comments("vk_comments", { limit: 10, width: "520", attach: "*" });
+        }
 	};
 });
 
--- frontEnd/scripts/directives/loggedInPage.js
+++ frontEnd/scripts/directives/loggedInPage.js
@@ -1,41 +1,68 @@
-myApp.directive('loggedInPage', ['API', 'authorization','auth', function (API,authorization,auth){
+myApp.directive('loggedInPage', ['API', 'authorization', 'auth', function (API, authorization, auth) {
     return {
         restrict: 'A',
         replace: true,
-        templateUrl:"partials/LoggedInPage.html",
-        scope:{
-            user:'='
+        templateUrl: "partials/LoggedInPage.html",
+        scope: {
+            user: '='
         },
-        controller:function($scope, $element){
-            $scope.$watch('user',function(value){
+        controller: function ($scope, $element) {
+
+            var getDate = function () {
+                var s;
+                var t = new Date();
+                var y = t.getFullYear();
+                var d = t.getDate();
+                var mon = t.getMonth();
+                switch (mon) {
+                    case 0: s = "января"; break;
+                    case 1: s = "февраля"; break;
+                    case 2: s = "марта"; break;
+                    case 3: s = "апреля"; break;
+                    case 4: s = "мае"; break;
+                    case 5: s = "июня"; break;
+                    case 6: s = "июля"; break;
+                    case 7: s = "августа"; break;
+                    case 8: s = "сентября"; break;
+                    case 9: s = "октября"; break;
+                    case 10: s = "ноября"; break;
+                    case 11: s = "декабря"; break;
+                }
+                return d + " " + s + " " + y;
+
+            }
+
+            $scope.currentDate = getDate();
+
+            $scope.$watch('user', function (value) {
                 if (value) $scope.unSelectTabs();
             })
 
             $scope.tabs = [
-                {Id:1, Name:'График платежей', presentationLink:'presentations/paymentPage.html'},
-                {Id:2, Name:'Карта фермеров', presentationLink:'presentations/mapPage.html'},
-                {Id:3, Name:'Новости', presentationLink:'presentations/forumPage.html'},
+                { Id: 1, Name: 'График платежей', presentationLink: 'presentations/paymentPage.html' },
+                { Id: 2, Name: 'Карта фермеров', presentationLink: 'presentations/mapPage.html' },
+                { Id: 3, Name: 'Новости', presentationLink: 'presentations/forumPage.html' },
             ];
 
-            $scope.unSelectTabs = function(){
-                for(var x in $scope.tabs){
+            $scope.unSelectTabs = function () {
+                for (var x in $scope.tabs) {
                     $scope.tabs[x].selected = false;
                 }
             }
 
-            $scope.newSelect = function(id){
-                for(var x in $scope.tabs){
+            $scope.newSelect = function (id) {
+                for (var x in $scope.tabs) {
                     $scope.tabs[x].selected = false;
-                    if($scope.tabs[x].Id === id){
+                    if ($scope.tabs[x].Id === id) {
                         $scope.tabs[x].selected = true;
                         $scope.user = '';
-                        if (!$scope.tabs[x].presentation){
+                        if (!$scope.tabs[x].presentation) {
                             var i = x;
-                            API.getPresentation($scope.tabs[x].presentationLink).then(function(data){
+                            API.getPresentation($scope.tabs[x].presentationLink).then(function (data) {
                                 $scope.tabs[i].presentation = data.data;
                                 $scope.selectedTab = $scope.tabs[i];
                             });
-                        }    
+                        }
                         else $scope.selectedTab = $scope.tabs[x];
                     }
                 };
@@ -46,4 +73,4 @@ myApp.directive('loggedInPage', ['API', 'authorization','auth', function (API,au
             //$scope.showProfile = true;
         }
     }
-}]);
+} ]);

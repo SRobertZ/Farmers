79d2b3c1f88f18efd51de81b5e27a10618d21093
RobertSabiryanov <robert.sabiryanov@gmail.com>
2016-02-25 22:45:56 +0500

    config changed

--- frontEnd/scripts/app.js
+++ frontEnd/scripts/app.js
@@ -16,7 +16,7 @@ app.config(['$httpProvider', function ($httpProvider) {
 
 app.factory('configuration', function () {
     var config = {};
-    config.url = 'http://ec2-54-200-232-69.us-west-2.compute.amazonaws.com:3000/';
+    config.url = 'http://fermerrb.ru:3000/';
     return config;
 });
 
